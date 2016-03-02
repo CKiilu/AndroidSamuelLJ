0c354bbc03a810994c71957c46174cb7cb3702f3
CKiilu <cndeti@gmail.com>
2016-03-02 11:32:16 +0300

    Splash activity done

--- app/src/main/java/com/scurrae/chris/androidsamuellj/Splash.java
+++ app/src/main/java/com/scurrae/chris/androidsamuellj/Splash.java
@@ -1,15 +1,37 @@
 package com.scurrae.chris.androidsamuellj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by chris on 3/2/16.
  */
 public class Splash extends AppCompatActivity{
+    private static int SPLASH_TIME_OUT=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer.
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash.this, Main.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
