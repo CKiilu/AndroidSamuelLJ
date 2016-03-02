4edeee97aa8b033f21e171f6dec0a2dceb0c223d
CKiilu <cndeti@gmail.com>
2016-03-02 10:27:03 +0300

    Basic activities and views

---
+++ app/src/main/java/com/scurrae/chris/androidsamuellj/Splash.java
@@ -0,0 +1,15 @@
+package com.scurrae.chris.androidsamuellj;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chris on 3/2/16.
+ */
+public class Splash extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
